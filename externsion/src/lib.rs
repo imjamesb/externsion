22dbc230386d56925fd713404028f8550e191c24
James Bradlee <post@jamesb.no>
2022-10-30 21:36:30 +0100

    restructure Repository

--- externsion/src/lib.rs
+++ externsion/src/lib.rs
@@ -131,26 +131,25 @@ pub type RequiredDependency = bool;
 pub type DependencyIdentifier = ExtensionIdentifier;
 
 pub struct ExtensionRepository<'a, T: BaseExtension + Send + Sync> {
-	pub installed_extensions: Vec<T>,
-	pub visited_extensions: HashMap<&'static str, &'a ExtensionIdentifier>,
+	pub installed_extensions: Vec<&'a T>,
+	pub loaded_extensions: HashMap<&'static str, (&'a ExtensionIdentifier, &'a T)>,
 	pub queued_extensions: Vec<&'a ExtensionManifest<T>>,
 	pub duplicates: HashMap<ExtensionName, Vec<&'a ExtensionIdentifier>>,
-	pub version_mismatches: HashMap<&'a ExtensionIdentifier, (&'a ExtensionDependency, InstalledVersion)>,
-	pub pending_dependency:
-		HashMap<DependencyName, Vec<(&'a ExtensionManifest<T>, ExpectedVersion, RequiredDependency)>>,
-	pub pending: HashMap<&'a ExtensionIdentifier, u32>,
+	pub version_mismatches: HashMap<&'a DependencyIdentifier, Vec<(ExpectedVersion, ExtensionIdentifier)>>,
+	pub pending_dependency: HashMap<DependencyName, Vec<(&'a ExtensionIdentifier, &'a ExtensionDependency)>>,
+	pub pending_counter: HashMap<&'a ExtensionIdentifier, u32>,
 }
 
 impl<T: BaseExtension + Send + Sync> ExtensionRepository<'_, T> {
 	pub fn new() -> Self {
 		ExtensionRepository {
 			installed_extensions: Vec::new(),
-			visited_extensions: HashMap::new(),
+			loaded_extensions: HashMap::new(),
 			queued_extensions: Vec::new(),
 			duplicates: HashMap::new(),
 			version_mismatches: HashMap::new(),
 			pending_dependency: HashMap::new(),
-			pending: HashMap::new(),
+			pending_counter: HashMap::new(),
 		}
 	}
 }
