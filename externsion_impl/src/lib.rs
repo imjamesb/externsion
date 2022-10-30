f8b97fbf76577e90d98b513a671671160eb932c2
James Bradlee <post@jamesb.no>
2022-10-30 21:17:57 +0100

    Added some documentation and a new flush field on the Repository trait

--- externsion_impl/src/lib.rs
+++ externsion_impl/src/lib.rs
@@ -1,19 +1,29 @@
 mod errors;
 
+use errors::InstallError;
 use externsion::*;
 
 pub trait Repository<'a, T: BaseExtension + Send + Sync> {
 	/// Queue a manifest to be added to the repository. It will only be queued and not touched
 	/// until you you use `<Repository>.install()`
 	fn queue(&mut self, manifest: &'a ExtensionManifest<T>);
-	fn install(&mut self) -> Result<(), ()>;
+	/// Flush all extensions in the queue and attempt to activate them into your extension system.
+	/// Returns a vector of those extension's identifiers that could be activated from the queue.
+	fn flush(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError>;
+	/// Flush all extensions and returns an error if there are missing dependencies or mismatches
+	/// dependency versions. Returns a vector of those extension's identifiers that could be
+	/// activated from the queue.
+	fn install(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError>;
 }
 
 impl<'a, T: BaseExtension + Send + Sync> Repository<'a, T> for ExtensionRepository<'a, T> {
 	fn queue(&mut self, manifest: &'a ExtensionManifest<T>) {
 		self.queued_extensions.push(manifest);
 	}
-	fn install(&mut self) -> Result<(), ()> {
+	fn flush(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError> {
+		todo!();
+	}
+	fn install(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError> {
 		todo!();
 	}
 }
