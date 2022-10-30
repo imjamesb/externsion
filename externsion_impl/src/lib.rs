d66c8e5a8b838ccf531a62bfe8a93d468708c23a
James Bradlee <post@jamesb.no>
2022-10-30 20:34:52 +0100

    fixed type param and lifetime param in Repository trait

--- externsion_impl/src/lib.rs
+++ externsion_impl/src/lib.rs
@@ -5,8 +5,8 @@ use externsion::*;
 pub trait Repository<'a, T: BaseExtension + Send + Sync> {
 	/// Queue a manifest to be added to the repository. It will only be queued and not touched
 	/// until you you use `<Repository>.install()`
-	fn queue(self: &mut ExtensionRepository<'a, T>, manifest: &'a ExtensionManifest<T>);
-	fn install(self: &mut ExtensionRepository) -> Result<()>;
+	fn queue(&mut self, manifest: &'a ExtensionManifest<T>);
+	fn install(&mut self) -> Result<(), ()>;
 }
 
-impl<'a, T: BaseExtension + Send + Sync> Repository for ExtensionRepository<'a, T> {}
+impl<'a, T: BaseExtension + Send + Sync> Repository<'a, T> for ExtensionRepository<'a, T> {}
