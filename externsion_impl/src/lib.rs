9cb004385a2237e13bf122a15c2f8a67c1eca24a
James Bradlee <post@jamesb.no>
2022-10-30 21:24:13 +0100

    Added push function to the Repository trait.

--- externsion_impl/src/lib.rs
+++ externsion_impl/src/lib.rs
@@ -7,6 +7,15 @@ pub trait Repository<'a, T: BaseExtension + Send + Sync> {
 	/// Queue a manifest to be added to the repository. It will only be queued and not touched
 	/// until you you use `<Repository>.install()`
 	fn queue(&mut self, manifest: &'a ExtensionManifest<T>);
+	/// Attempt to push a manifest manually directly onto the repository. Returns `None` if the
+	/// extension couldn't be installed immediately and is depending on another extension that hasn't
+	/// loaded yet. Returns `Some(ExtensionIdentifier)` if the extension was installed and activated
+	/// without issues. Returns `Err(InstallError)` if there is a version mismatch or if the manifest is
+	/// a duplicate.
+	fn push(
+		&mut self,
+		manifest: &'a ExtensionManifest<T>,
+	) -> Result<Option<&'a ExtensionIdentifier>, InstallError>;
 	/// Flush all extensions in the queue and attempt to activate them into your extension system.
 	/// Returns a vector of those extension's identifiers that could be activated from the queue.
 	fn flush(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError>;
@@ -19,6 +28,12 @@ pub trait Repository<'a, T: BaseExtension + Send + Sync> {
 impl<'a, T: BaseExtension + Send + Sync> Repository<'a, T> for ExtensionRepository<'a, T> {
 	fn queue(&mut self, manifest: &'a ExtensionManifest<T>) {
 		self.queued_extensions.push(manifest);
+	}
+	fn push(
+		&mut self,
+		manifest: &'a ExtensionManifest<T>,
+	) -> Result<Option<&'a ExtensionIdentifier>, InstallError> {
+		todo!();
 	}
 	fn flush(&mut self) -> Result<&'a Vec<&'a ExtensionIdentifier>, InstallError> {
 		todo!();
