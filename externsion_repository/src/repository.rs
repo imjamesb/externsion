0d99e6015edcc680d1200eed42ba670dbb031b61
James Bradlee <post@jamesb.no>
2022-11-04 06:17:16 +0100

    restructure externsion_repository

---
+++ externsion_repository/src/repository.rs
@@ -0,0 +1,73 @@
+use externsion::{
+	ExtensionIdentifier, ExtensionManifest, ExtensionSource,
+	GetSourceError, InnerRepository, InstallError, QueueError,
+	Repository as ExtRepository, RepositoryOperation, SetSourceError,
+	UnloadError,
+};
+
+pub struct Repository<'a> {
+	inner: InnerRepository<'a>,
+}
+
+impl Repository<'_> {
+	pub fn new<'a>() -> Repository<'a> {
+		Repository {
+			inner: InnerRepository::new(),
+		}
+	}
+}
+
+impl<'a> ExtRepository<'a> for Repository<'a> {
+	fn queue(
+		&mut self,
+		manifest: &'a externsion::ExtensionManifest,
+		source: Option<&'a str>,
+	) -> Result<(), QueueError<'a>> {
+		todo!();
+	}
+
+	fn unqueue(
+		&mut self,
+		identifier: &'a ExtensionIdentifier,
+	) -> Result<ExtensionManifest, QueueError<'a>> {
+		todo!();
+	}
+
+	fn push(
+		&mut self,
+		manifest: &'a externsion::ExtensionManifest,
+		source: Option<&'a str>,
+	) -> Result<&'a ExtensionIdentifier, InstallError<'a>> {
+		todo!();
+	}
+
+	fn flush(&mut self) -> RepositoryOperation<'a> {
+		todo!();
+	}
+
+	fn install(&mut self) -> RepositoryOperation<'a> {
+		todo!();
+	}
+
+	fn set_source(
+		&mut self,
+		identifier: &'a ExtensionIdentifier,
+		source: ExtensionSource<'a>,
+	) -> Result<(), SetSourceError<'a>> {
+		todo!();
+	}
+
+	fn get_source(
+		&self,
+		identifier: &'a ExtensionIdentifier,
+	) -> Result<Option<ExtensionSource<'a>>, GetSourceError<'a>> {
+		todo!();
+	}
+
+	fn unload(
+		&mut self,
+		identifier: &'a ExtensionIdentifier,
+	) -> Result<ExtensionManifest, UnloadError<'a>> {
+		todo!();
+	}
+}
