9d5c5851506cdf1dde5f075dc980cd710f5512d2
James Bradlee <post@jamesb.no>
2022-11-05 12:00:35 +0100

    Fixed build errors in queue_error.rs

--- externsion/src/errors/queue_error.rs
+++ externsion/src/errors/queue_error.rs
@@ -1,24 +1,32 @@
 use std::{error::Error, fmt::Display};
 
+use crate::{ExtensionIdentifier, ExtensionSource};
+
 #[derive(Debug)]
 pub struct QueueError<'a> {
-	description: &'a str,
+	queued: (&'a ExtensionIdentifier, Option<ExtensionSource<'a>>),
+	attempted: (&'a ExtensionIdentifier, Option<ExtensionSource<'a>>),
 }
 
 impl<'a> QueueError<'a> {
-	pub fn new(description: &'a str) -> QueueError<'a> {
-		QueueError { description }
+	pub fn new(
+		queued: (&'a ExtensionIdentifier, Option<ExtensionSource<'a>>),
+		attempted: (&'a ExtensionIdentifier, Option<ExtensionSource<'a>>),
+	) -> QueueError<'a> {
+		QueueError { queued, attempted }
 	}
 }
 
 impl Display for QueueError<'_> {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}", self.description)
+		write!(f, "An extension with the same identifier ({}) has already been queued or registered{}.{}", self.queued.0, match self.queued.1 {
+			Some(source) => format!(" (from {})", source),
+			None => "".to_string()
+		}, match self.attempted.1 {
+			Some(source) => format!(" Loading from {}", source),
+			None => "".to_string()
+		})
 	}
 }
 
-impl Error for QueueError<'_> {
-	fn description(&self) -> &str {
-		self.description
-	}
-}
+impl Error for QueueError<'_> {}
