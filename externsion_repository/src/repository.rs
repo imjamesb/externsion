cb86d53d85899f2569bc66435e04c9e947545680
James Bradlee <post@jamesb.no>
2022-11-05 12:05:21 +0100

    Remove source methods and implement queue method

--- externsion_repository/src/repository.rs
+++ externsion_repository/src/repository.rs
@@ -1,8 +1,7 @@
 use externsion::{
 	ExtensionData, ExtensionIdentifier, ExtensionManifest,
-	ExtensionSource, GetSourceError, InnerRepository, InstallError,
-	QueueError, Repository as ExtRepository, RepositoryOperation,
-	SetSourceError, UnloadError,
+	ExtensionSource, InnerRepository, InstallError, QueueError,
+	Repository as ExtRepository, RepositoryOperation, UnloadError,
 };
 
 pub struct Repository<'a> {
@@ -18,13 +17,43 @@ impl Repository<'_> {
 }
 
 impl<'a> ExtRepository<'a> for Repository<'a> {
+	// Note that any failed queued extensions will have to be
+	// unloaded manually.
 	fn queue(
 		&mut self,
 		manifest: &'a ExtensionManifest,
-		source: Option<&'a str>,
-		data: Option<&'a ExtensionData>,
+		source: Option<ExtensionSource<'a>>,
+		data: Option<ExtensionData>,
 	) -> Result<&'a ExtensionIdentifier, QueueError<'a>> {
-		todo!();
+		if self.inner.queued_extensions.contains(&&manifest.identifier)
+			|| self
+				.inner
+				.pending_extensions_r
+				.contains_key(&manifest.identifier)
+		{
+			Err(QueueError::new(
+				(
+					&manifest.identifier,
+					match self.inner.sources.get(&manifest.identifier) {
+						Some(source) => Some(source),
+						_ => None,
+					},
+				),
+				(&manifest.identifier, source),
+			))
+		} else {
+			self.inner.queued_extensions.push(&manifest.identifier);
+			if let Some(data) = data {
+				self.inner.queued_data.insert(&manifest.identifier, data);
+			}
+			if let Some(source) = source {
+				self.inner.sources.insert(&manifest.identifier, source);
+			}
+			self.inner
+				.queued_manifests
+				.insert(&manifest.identifier, manifest);
+			Ok(&manifest.identifier)
+		}
 	}
 
 	fn unqueue(
@@ -48,21 +77,6 @@ impl<'a> ExtRepository<'a> for Repository<'a> {
 	}
 
 	fn install(&mut self) -> RepositoryOperation<'a> {
-		todo!();
-	}
-
-	fn set_source(
-		&mut self,
-		identifier: &'a ExtensionIdentifier,
-		source: ExtensionSource<'a>,
-	) -> Result<(), SetSourceError<'a>> {
-		todo!();
-	}
-
-	fn get_source(
-		&self,
-		identifier: &'a ExtensionIdentifier,
-	) -> Result<Option<ExtensionSource<'a>>, GetSourceError<'a>> {
 		todo!();
 	}
 
